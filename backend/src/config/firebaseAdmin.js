6b5a883f13c188adff45f6bc3ca4762998f23cc8
Nam Nguyen <nguyenminhnam1739@gmail.com>
2025-06-16 12:18:51 +0700

    fix config firebase env

--- backend/src/config/firebaseAdmin.js
+++ backend/src/config/firebaseAdmin.js
@@ -1,9 +1,15 @@
-import admin from 'firebase-admin';
 import fs from 'fs';
+import admin from 'firebase-admin';
 
-const serviceAccount = JSON.parse(
-    fs.readFileSync(new URL('./serviceAccountKey.json', import.meta.url))
-);
+// Nếu đang chạy ở môi trường production và chưa có file thì tạo
+const keyFilePath = './src/config/serviceAccountKey.json';
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT && !fs.existsSync(keyFilePath)) {
+    fs.writeFileSync(keyFilePath, process.env.FIREBASE_SERVICE_ACCOUNT);
+}
+
+// Đọc file như cũ
+const serviceAccount = JSON.parse(fs.readFileSync(keyFilePath, 'utf8'));
 
 if (!admin.apps.length) {
     admin.initializeApp({
