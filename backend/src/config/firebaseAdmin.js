c41e3b7cbe8b0b3335eadb56efacb51715880064
Nam Nguyen <nguyenminhnam1739@gmail.com>
2025-06-16 20:01:20 +0700

    fix auth firebase

--- backend/src/config/firebaseAdmin.js
+++ backend/src/config/firebaseAdmin.js
@@ -1,20 +1,23 @@
-// import fs from 'fs';
-// import admin from 'firebase-admin';
+import { config } from 'dotenv';
+import admin from 'firebase-admin';
+config();
 
-// // Nếu đang chạy ở môi trường production và chưa có file thì tạo
-// const keyFilePath = './src/config/serviceAccountKey.json';
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+}
 
-// if (process.env.FIREBASE_SERVICE_ACCOUNT && !fs.existsSync(keyFilePath)) {
-//     fs.writeFileSync(keyFilePath, process.env.FIREBASE_SERVICE_ACCOUNT);
-// }
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
-// // Đọc file như cũ
-// const serviceAccount = JSON.parse(fs.readFileSync(keyFilePath, 'utf8'));
-
-// if (!admin.apps.length) {
-//     admin.initializeApp({
-//         credential: admin.credential.cert(serviceAccount),
-//     });
-// }
-
-// export default admin;
+export default admin; 
